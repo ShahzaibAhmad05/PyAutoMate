ba7663fef6bcd4153ad17cfbb6d6546c92cd703d
Shahzaib <shahzaibahmad6789@gmail.com>
2025-11-29 19:28:45 +0500

    Cleanup imports in mainTool (#6)

--- mainTool.py
+++ mainTool.py
@@ -1,49 +1,24 @@
-""" All Imports """
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QHBoxLayout
-from PyQt5.QtCore import Qt, QTimer, QEventLoop
-from PyQt5.QtGui import QPixmap
-import logging
-import os
-import inspect
-import pickle
-from modules.sysUtils import sleep_for, close_other_instances
+# PyQt5 imports
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QDialog, QMessageBox, QMenu
+from PyQt5.QtCore import Qt, QPoint, QRect, QSize, QPropertyAnimation, QEasingCurve
+from PyQt5.QtGui import QIcon, QColor, QPainter
+
+# python libraries
+import os, sys, shutil, pyautogui
+
+# self-defined modules
+from modules.getters import get_settings, get_logo_path
+from modules.utils import save_script, enable_dragging, generateRandomID, add_spaces_for_context_menu
+from modules.sysUtils import sleep_for
 from Assistant import GlobalTextBox
-import threading
-import json
-import time
-import sys
-import shutil
-import pyautogui
-import keyboard
-import mouse
-from typing import Callable
-import pyperclip
-from io import BytesIO
-import random
-from PIL import Image
-import webbrowser
-import psutil
-from pygetwindow import getActiveWindow, getWindowsWithTitle
-from PyQt5.QtWidgets import (QPushButton,
-                            QDialog, QMessageBox, QVBoxLayout,
-                            QTextEdit, QFileDialog, QRadioButton,
-                            QButtonGroup, QMenu, QInputDialog, QLineEdit,
-                            QCheckBox, QSpinBox)
-from PyQt5.QtCore import QPoint, QRect, QSize, QPropertyAnimation, QEasingCurve
-from PyQt5.QtGui import QIcon, QTextCharFormat, QSyntaxHighlighter, QColor, QPainter, QCursor, QFont
-# self-defined functions
-from modules.getters import get_settings, get_logo_path, get_icon_path, get_script_path
-from modules.utils import save_script, delete_script, load_script, enable_dragging, generateRandomID, add_spaces_for_context_menu
+
+# UI elements
 from ui.settings import Settings
-from ui.scriptingDialog import ScriptingDialog
-from ui.scriptOption import ScriptOption
-from ui.customTextEdit import CustomTextEdit
 from ui.addButtonWindow import AddButtonWindow
 from ui.draggableButton import DraggableButton
-from modules.styling import (push_button_disabled_stylesheet, push_button_stylesheet,
-                            context_menu_stylesheet, dialog_window_stylesheet)
 
-pyautogui.FAILSAFE = False
+# CSS Styling functions
+from modules.styling import (push_button_stylesheet, context_menu_stylesheet)
 
 # start of MainTool Class
 class MainTool(QMainWindow):
@@ -79,22 +54,21 @@ class MainTool(QMainWindow):
             if self.app_theme == 'light' else QColor(30, 30, 30))
 
         # initialize the user interface
-        self.init_ui()
+        self.initUI()
         enable_dragging(self)
+        enable_dragging(self.logo_button)
         # Assistant control variables
         self.assistant_text_enabled = True
         self.floating_textbox = GlobalTextBox(self)
 
 
-    def init_ui(self) -> None:
+    def initUI(self) -> None:
         """ Initialize the UI components of the main window """
         # setup logo button
         self.logo_button = QPushButton(self)
         self.logo_button.setIcon(QIcon(get_logo_path()))
         self.logo_button.setStyleSheet("QPushButton { border: none; }")
         self.logo_button.mouseDoubleClickEvent = self.open_tool
-        self.logo_button.mousePressEvent = self.mousePressEvent
-        self.logo_button.mouseMoveEvent = self.mouseMoveEvent
         self.place_button(self.logo_button, 0, 0)
 
         # setup app title
