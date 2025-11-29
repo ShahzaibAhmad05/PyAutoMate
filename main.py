29cc78120fecb8059febe85956c3160475908065
Shahzaib <shahzaibahmad6789@gmail.com>
2025-11-29 19:40:36 +0500

    remove: redundant imports in main (#6)

--- main.py
+++ main.py
@@ -1,52 +1,15 @@
-""" All Imports """
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QHBoxLayout
-from PyQt5.QtCore import Qt, QTimer, QEventLoop
-from PyQt5.QtGui import QPixmap
-import logging
-import os
-import inspect
-import pickle
-from modules.sysUtils import sleep_for, close_other_instances
-from Assistant import GlobalTextBox
-import threading
-import json
-import time
-import sys
-import shutil
-import pyautogui
-import keyboard
-import mouse
-from typing import Callable
-import pyperclip
-from io import BytesIO
-import random
-from PIL import Image
-import webbrowser
-import psutil
-from pygetwindow import getActiveWindow, getWindowsWithTitle
-from PyQt5.QtWidgets import (QPushButton,
-                            QDialog, QMessageBox, QVBoxLayout,
-                            QTextEdit, QFileDialog, QRadioButton,
-                            QButtonGroup, QMenu, QInputDialog, QLineEdit,
-                            QCheckBox, QSpinBox)
-from PyQt5.QtCore import QPoint, QRect, QSize, QPropertyAnimation, QEasingCurve
-from PyQt5.QtGui import QIcon, QTextCharFormat, QSyntaxHighlighter, QColor, QPainter, QCursor, QFont
-# self-defined functions
-from modules.getters import get_settings, get_logo_path, get_icon_path, get_script_path
-from modules.utils import save_script, delete_script, load_script, enable_dragging, generateRandomID, add_spaces_for_context_menu
-from ui.settings import Settings
-from ui.scriptingDialog import ScriptingDialog
-from ui.scriptOption import ScriptOption
-from ui.customTextEdit import CustomTextEdit
-from ui.addButtonWindow import AddButtonWindow
-from ui.draggableButton import DraggableButton
-from modules.styling import (push_button_disabled_stylesheet, push_button_stylesheet,
-                            context_menu_stylesheet, dialog_window_stylesheet)
+# PyQt5 imports
+from PyQt5.QtWidgets import QMessageBox, QApplication
+from PyQt5.QtGui import QIcon
+
+# python libraries
+import os, sys
+
+# self-defined modules
+from modules.sysUtils import close_other_instances
+from modules.getters import get_logo_path
 from mainTool import MainTool
 
-pyautogui.FAILSAFE = False
-
-""" Program Starts From Here After Imports """
 
 def main():
     # check for any missing files and stylesheets
